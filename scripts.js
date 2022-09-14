79a6c206b765cea02dfde080e4b65e78d112224a
gwumex <42099116+gwumex@users.noreply.github.com>
2022-09-14 09:52:20 +0100

    carting website

---
+++ scripts.js
@@ -0,0 +1,45 @@
+let products = [
+    {
+        name:'Green Hoodie',
+        tag:'greenhoodie',
+        price:700,
+        inCart:0
+    },
+    {
+        name:'Black Hoodie',
+        tag:'blackhoodie',
+        price:750,
+        inCart:0
+    },
+
+    {
+        name:'Orange Hoodie',
+        tag:'orangehoodie',
+        price:720,
+        inCart:0
+    },
+
+    {
+        name:'Green-o Hoodie',
+        tag:'green-ohoodie',
+        price:800,
+        inCart:0
+    },
+
+    {
+        name:'Grey Hoodie',
+        tag:'greyhoodie',
+        price:850,
+        inCart:0
+    },
+
+    {
+        name:'Yellow Hoodie',
+        tag:'yellowhoodie',
+        price:700,
+        inCart:0
+    }
+    
+];
+
+
