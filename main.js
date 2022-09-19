1b6e1c0772e0c3fc8ce04d1b76eda49a2f99e35b
gwumex <42099116+gwumex@users.noreply.github.com>
2022-09-20 00:40:04 +0100

    display items in cart

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let products = [
         tag:'greenhoodie',
         image: "./images/hd1.jpg",
         price:700,
-        inCart:0
+        inCart:0,
+        inStock: 7
     },
     {
         id: '1',
@@ -13,7 +14,8 @@ let products = [
         tag:'blackhoodie',
         image: "./images/hd2.jpg",
         price:750,
-        inCart:0
+        inCart:0,
+        inStock: 12
     },
 
     {
@@ -22,7 +24,8 @@ let products = [
         tag:'orangehoodie',
         image: "./images/hd3.jpg",
         price:720,
-        inCart:0
+        inCart:0,
+        inStock: 13
     },
 
     {
@@ -31,7 +34,8 @@ let products = [
         tag:'green-ohoodie',
         image: "./images/hd4.jpg",
         price:800,
-        inCart:0
+        inCart:0,
+        inStock: 14
     },
 
     {
@@ -40,7 +44,8 @@ let products = [
         tag:'greyhoodie',
         image: "./images/hd5.jpg",
         price:850,
-        inCart:0
+        inCart:0,
+        inStock: 15
     },
 
     {
@@ -49,7 +54,8 @@ let products = [
         tag:'yellowhoodie',
         image: "./images/hd6.jpg",
         price:700,
-        inCart:0
+        inCart:0,
+        inStock: 18
     }
     
 ];
@@ -92,9 +98,9 @@ function displayItems(){
 }
 //onLoadcart
 function onLoadCartNumbers() {
-    let productNumber = sessionStorage.getItem('itemsInCart');
+    let productNumber = localStorage.getItem('itemsInCart');
     if( productNumber){
-        document.querySelector('.cartBtn span').textContent = productNumber;
+        document.getElementById('number').textContent = productNumber;
     }
 
 }
@@ -120,15 +126,15 @@ function addToCart(){
 //add number to cart
 
 function addCartNumber(product){
-    let productNumber = sessionStorage.getItem('itemsInCart'); 
+    let productNumber = localStorage.getItem('itemsInCart'); 
     productNumber = parseInt(productNumber);
 
-   let cartNumber = document.querySelector('.cartBtn span');
+   let cartNumber = document.getElementById('number');
     if(productNumber) {
-        sessionStorage.setItem('itemsInCart', productNumber + 1)
+        localStorage.setItem('itemsInCart', productNumber + 1)
         cartNumber.textContent = productNumber + 1;
     }else{
-        sessionStorage.setItem('itemsInCart', 1);
+        localStorage.setItem('itemsInCart', 1);
         cartNumber.textContent = productNumber + 1;
     }
     setItems(product);
@@ -136,7 +142,7 @@ function addCartNumber(product){
 
 // add carted items to local storage
 function setItems(product) {
-    let cartItems = sessionStorage.getItem('productsInCart');
+    let cartItems = localStorage.getItem('productsInCart');
     cartItems = JSON.parse(cartItems);
     
 
@@ -157,24 +163,77 @@ function setItems(product) {
     }
     console.log(product)
     
-    sessionStorage.setItem("productsInCart",JSON.stringify(cartItems));
+    localStorage.setItem("productsInCart",JSON.stringify(cartItems));
 }
 
 ///total cart cost
 function totalCost(product){
-    let cartCost = sessionStorage.getItem('totalCost');
+    let cartCost = localStorage.getItem('totalCost');
     console.log("My cartCost is", cartCost);
     console.log(typeof cartCost);
 
     if(cartCost != null){
         cartCost = parseInt(cartCost);
-        sessionStorage.setItem("totalCost", cartCost + product.price);
+        localStorage.setItem("totalCost", cartCost + product.price);
     }else{
-        sessionStorage.setItem("totalCost",product.price);
+        localStorage.setItem("totalCost",product.price);
+    }
+}
+//display total cost of items in cart
+function displayTotalCost(){
+    let cartCost = localStorage.getItem('totalCost');
+    cartCost = JSON.parse(cartCost)
+    console.log("this is me" + cartCost)
+
+    let costContainer = document.getElementById("totalcost");
+
+
+    if(cartCost && costContainer) {
+        costContainer.textContent = cartCost;
+    }else{
+        return onLoadCartNumbers;
     }
 }
 
-navBar();
-displayItems();
+// this function display selected content in cart page
+
+function displayCart(){
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems)
+    let cartContainer = document.querySelector(".cart");
+    if (cartItems && cartContainer ){
+        console.log("running");
+        cartContainer.innerHTML = ''; 
+        Object.values(cartItems).map(item => {
+            cartContainer.innerHTML += `
+            <div class="cartItem" id="cartItem">
+            <input type="checkbox" checked disabled name="add" id="add">
+            <div class="cartImgBox" id="cartImgBox">
+                <img src="${item.image}" alt="" class="cartImg">
+            </div>
+            <div class="cartDetails">
+                <h2 class="itemName">
+                    ${item.name}
+                </h2>
+                <span class="itemLeft">${item.inStock} left in stock</span>
+                <span class="inCart">${ item.inCart} added to cart</span>
+            </div>
+            <span class="price">${item.price}$</span>
+            
+
+            </div>
+            <div class="divider"></div>
+        ` 
+        });  
+    }else{
+        
+
+    }
+} 
+displayCart();
+displayTotalCost();
 onLoadCartNumbers();
+navBar();
+
+displayItems();
 addToCart();
