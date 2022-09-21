5ce64b273b26ff0975dd9bef322084ad940c20a4
gwumex <42099116+gwumex@users.noreply.github.com>
2022-09-21 11:32:47 +0100

    clear cart button working

--- main.js
+++ main.js
@@ -1,92 +1,92 @@
 let products = [
     {
         id: '0',
-        name:'Green Hoodie',
-        tag:'greenhoodie',
+        name: 'Green Hoodie',
+        tag: 'greenhoodie',
         image: "./images/hd1.jpg",
-        price:700,
-        inCart:0,
+        price: 700,
+        inCart: 0,
         inStock: 7
     },
     {
         id: '1',
-        name:'Black Hoodie',
-        tag:'blackhoodie',
+        name: 'Black Hoodie',
+        tag: 'blackhoodie',
         image: "./images/hd2.jpg",
-        price:750,
-        inCart:0,
+        price: 750,
+        inCart: 0,
         inStock: 12
     },
 
     {
         id: '2',
-        name:'Orange Hoodie',
-        tag:'orangehoodie',
+        name: 'Orange Hoodie',
+        tag: 'orangehoodie',
         image: "./images/hd3.jpg",
-        price:720,
-        inCart:0,
+        price: 720,
+        inCart: 0,
         inStock: 13
     },
 
     {
         id: '3',
-        name:'Green-o Hoodie',
-        tag:'green-ohoodie',
+        name: 'Green-o Hoodie',
+        tag: 'green-ohoodie',
         image: "./images/hd4.jpg",
-        price:800,
-        inCart:0,
+        price: 800,
+        inCart: 0,
         inStock: 14
     },
 
     {
         id: '4',
-        name:'Grey Hoodie',
-        tag:'greyhoodie',
+        name: 'Grey Hoodie',
+        tag: 'greyhoodie',
         image: "./images/hd5.jpg",
-        price:850,
-        inCart:0,
+        price: 850,
+        inCart: 0,
         inStock: 15
     },
 
     {
         id: '5',
-        name:'Yellow Hoodie',
-        tag:'yellowhoodie',
+        name: 'Yellow Hoodie',
+        tag: 'yellowhoodie',
         image: "./images/hd6.jpg",
-        price:700,
-        inCart:0,
+        price: 700,
+        inCart: 0,
         inStock: 18
     }
-    
+
 ];
 
-function navBar (){
-window.addEventListener("resize", function() {
-    if (window.matchMedia("(min-width: 600px)").matches) {
-        dropDownBox.style.display ="none";
-    } else {
-      return 1;
-    }
-  });
+function navBar() {
+    window.addEventListener("resize", function () {
+        if (window.matchMedia("(min-width: 600px)").matches) {
+            dropDownBox.style.display = "none";
+        } else {
+            return 1;
+        }
+    });
 
-var dropDownBtn = document.getElementById("dropDownBtn");
-var dropDownBox = document.getElementById("dropDownBox");
+    var dropDownBtn = document.getElementById("dropDownBtn");
+    var dropDownBox = document.getElementById("dropDownBox");
 
-dropDownBtn.addEventListener("click", function(){
-    if(dropDownBox.style.display === "block"){
-        dropDownBox.style.display = "none"
-    } else{
-        dropDownBox.style.display = "block"
-    }
-});
+    dropDownBtn.addEventListener("click", function () {
+        if (dropDownBox.style.display === "block") {
+            dropDownBox.style.display = "none"
+        } else {
+            dropDownBox.style.display = "block"
+        }
+    });
 }
 //display items
 
-function displayItems(){
+function displayItems() {
     let productContainer = document.getElementById('productsContainer');
     products.map(product => {
         productContainer.innerHTML = productContainer.innerHTML +
-        `<div class="cards" id="cards">
+            `<div class="cards" id="cards">
         <div class="productImg" id="productImg">
         <img src="${product.image}" alt="hd1"class="img" id="img">                       
         </div>
@@ -99,14 +99,14 @@ function displayItems(){
 //onLoadcart
 function onLoadCartNumbers() {
     let productNumber = localStorage.getItem('itemsInCart');
-    if( productNumber){
+    if (productNumber) {
         document.getElementById('number').textContent = productNumber;
     }
 
 }
 
 //add items to cart
-function addToCart(){
+function addToCart() {
     let addToCarts = document.querySelectorAll('#addToCart');
     // addToCarts.forEach(function(cartBtn){
     //     cartBtn.addEventListener('click', () => {
@@ -115,8 +115,8 @@ function addToCart(){
     // })
     //alternative method
 
-    for(let i=0; i < addToCarts.length; i++){
-        addToCarts[i].addEventListener('click',() => {
+    for (let i = 0; i < addToCarts.length; i++) {
+        addToCarts[i].addEventListener('click', () => {
             addCartNumber(products[i]);
             totalCost(products[i]);
         })
@@ -125,15 +125,15 @@ function addToCart(){
 
 //add number to cart
 
-function addCartNumber(product){
-    let productNumber = localStorage.getItem('itemsInCart'); 
+function addCartNumber(product) {
+    let productNumber = localStorage.getItem('itemsInCart');
     productNumber = parseInt(productNumber);
 
-   let cartNumber = document.getElementById('number');
-    if(productNumber) {
+    let cartNumber = document.getElementById('number');
+    if (productNumber) {
         localStorage.setItem('itemsInCart', productNumber + 1)
         cartNumber.textContent = productNumber + 1;
-    }else{
+    } else {
         localStorage.setItem('itemsInCart', 1);
         cartNumber.textContent = productNumber + 1;
     }
@@ -144,11 +144,11 @@ function addCartNumber(product){
 function setItems(product) {
     let cartItems = localStorage.getItem('productsInCart');
     cartItems = JSON.parse(cartItems);
-    
 
-    if (cartItems != null){
-       
-        if(cartItems[product.tag] == undefined){
+
+    if (cartItems != null) {
+
+        if (cartItems[product.tag] == undefined) {
             cartItems = {
                 ...cartItems,
                 [product.tag]: product
@@ -158,29 +158,29 @@ function setItems(product) {
     } else {
         product.inCart = 1;
         cartItems = {
-          [product.tag]: product
-       }
+            [product.tag]: product
+        }
     }
     console.log(product)
-    
-    localStorage.setItem("productsInCart",JSON.stringify(cartItems));
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
 ///total cart cost
-function totalCost(product){
+function totalCost(product) {
     let cartCost = localStorage.getItem('totalCost');
     console.log("My cartCost is", cartCost);
     console.log(typeof cartCost);
 
-    if(cartCost != null){
+    if (cartCost != null) {
         cartCost = parseInt(cartCost);
         localStorage.setItem("totalCost", cartCost + product.price);
-    }else{
-        localStorage.setItem("totalCost",product.price);
+    } else {
+        localStorage.setItem("totalCost", product.price);
     }
 }
 //display total cost of items in cart
-function displayTotalCost(){
+function displayTotalCost() {
     let cartCost = localStorage.getItem('totalCost');
     cartCost = JSON.parse(cartCost)
     console.log("this is me" + cartCost)
@@ -188,22 +188,22 @@ function displayTotalCost(){
     let costContainer = document.getElementById("totalcost");
 
 
-    if(cartCost && costContainer) {
+    if (cartCost && costContainer) {
         costContainer.textContent = cartCost;
-    }else{
+    } else {
         return onLoadCartNumbers;
     }
 }
 
 // this function display selected content in cart page
 
-function displayCart(){
+function displayCart() {
     let cartItems = localStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems)
     let cartContainer = document.querySelector(".cart");
-    if (cartItems && cartContainer ){
+    if (cartItems && cartContainer) {
         console.log("running");
-        cartContainer.innerHTML = ''; 
+        cartContainer.innerHTML = '';
         Object.values(cartItems).map(item => {
             cartContainer.innerHTML += `
             <div class="cartItem" id="cartItem">
@@ -216,25 +216,35 @@ function displayCart(){
                     ${item.name}
                 </h2>
                 <span class="itemLeft">${item.inStock} left in stock</span>
-                <span class="inCart">${ item.inCart} added to cart</span>
+                <span class="inCart">${item.inCart} added to cart</span>
             </div>
             <span class="price">${item.price}$</span>
             <i class="fa-sharp fa-solid"></i>
-            <i class="fa-sharp fa-solid fa-trash"></i>
-
+            <i class="fa-sharp fa-solid fa-trash " id="delItem" ></i>
             </div>
             <div class="divider"></div>
-        ` 
-        });  
-    }else{
+        `
+        });
+    } else {
         
 
+
     }
-} 
+}
+
+function deleteCartItems(){
+    localStorage.removeItem('productsInCart');
+}
+
+//clear cart function
+function clearCart(){
+    localStorage.clear();
+}
+
 displayCart();
 displayTotalCost();
 onLoadCartNumbers();
-navBar();
 
+navBar();
 displayItems();
 addToCart();
